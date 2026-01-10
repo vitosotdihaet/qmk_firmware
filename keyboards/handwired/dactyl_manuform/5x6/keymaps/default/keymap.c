31d5449310404d43de811617fd3f2b909f9648dc
vitosotdihaet <vitostikitos@gmail.com>
2026-01-10 14:01:47 +0300

    update

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
@@ -32,10 +32,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_RAISE] = LAYOUT_5x6(
           KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
-          _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NUM,KC_INS ,KC_SCRL,KC_MUTE,
-          _______,KC_MPRV,KC_MPLY,KC_MNXT,_______,_______,                        KC_LEFT,KC_DOWN,KC_UP ,KC_RGHT,_______,KC_VOLU,
+          _______,_______,_______,_______,_______,_______,                        _______,_______,KC_NUM ,KC_INS ,KC_SCRL,KC_MUTE,
+          _______,KC_MPRV,KC_MPLY,KC_MNXT,KC_PSCR,_______,                        KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,_______,KC_VOLU,
           _______,_______,_______,_______,KC_HOME,KC_END,                         _______,_______,_______,_______,_______,KC_VOLD,
-                          _______,_______,                                                        _______,_______,
+                          _______,_______,                                                        KC_F23 ,KC_F24 ,
                                                   _______,_______,        _______,_______,
                                                   _______,_______,        _______,_______,
                                                   _______,_______,        _______,_______
