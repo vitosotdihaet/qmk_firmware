5bb8c99233b73bd2cec9721cd1006021f37fad6c
vitosotdihaet <vitostikitos@gmail.com>
2026-04-02 17:59:38 +0300

    update

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
@@ -9,9 +9,9 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x6(
         KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSLS,
-        KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,_______,
+        KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_GRV ,
         KC_LCTL, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-        KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_LGUI,
+        KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,LGUI(KC_SPC),
                          KC_PGDN,KC_PGUP,                                                       KC_MINS, KC_EQL,
                                          LOWER,  _______,                        _______, RAISE,
                                          KC_LGUI,KC_LALT,                        KC_RBRC,KC_BSPC,
@@ -21,8 +21,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT_5x6(
         KC_GRV ,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
         _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
-        KC_BRIU,_______,_______,_______,_______,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
-        KC_BRID,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
+        _______,_______,_______,_______,_______,KC_LCBR,                        KC_RCBR, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_BRIU,
+        _______,_______,_______,_______,_______,KC_LPRN,                        KC_RPRN, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_BRID,
                         KC_HOME,KC_END,                                                          _______,_______,
                                                 _______,_______,        _______,_______,
                                                 _______,_______,        _______,_______,
