1d08a79242f08b422002607fcf00c29b55934d8e
vitosotdihaet <vitostikitos@gmail.com>
2026-04-07 14:54:45 +0300

    move right side a bit

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
@@ -8,10 +8,10 @@
 #define LOWER MO(_LOWER)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x6(
-        KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSLS,
-        KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_GRV ,
-        KC_LCTL, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-        KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,LGUI(KC_SPC),
+        KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_BSLS,      KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,
+        KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_GRV ,      KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,
+        KC_LCTL, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_QUOT,      KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,
+        KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         LGUI(KC_SPC), KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,
                          KC_PGDN,KC_PGUP,                                                       KC_MINS, KC_EQL,
                                          LOWER,  _______,                        _______, RAISE,
                                          KC_LGUI,KC_LALT,                        KC_RBRC,KC_BSPC,
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_LOWER] = LAYOUT_5x6(
-        KC_GRV ,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
+        KC_GRV ,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_DEL, KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,
         _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
         _______,_______,_______,_______,_______,KC_LCBR,                        KC_RCBR, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_BRIU,
         _______,_______,_______,_______,_______,KC_LPRN,                        KC_RPRN, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_BRID,
@@ -31,10 +31,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_RAISE] = LAYOUT_5x6(
-          KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
-          _______,_______,_______,_______,_______,_______,                        _______,_______,KC_NUM ,KC_INS ,KC_SCRL,KC_MUTE,
-          _______,KC_MPRV,KC_MPLY,KC_MNXT,KC_PSCR,_______,                        KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,_______,KC_VOLU,
-          _______,_______,_______,_______,KC_HOME,KC_END,                         MS_LEFT,MS_DOWN,MS_UP  ,MS_RGHT,_______,KC_VOLD,
+          KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F11 ,KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,
+          _______,_______,_______,_______,_______,_______,                        KC_MUTE,_______,_______,KC_NUM ,KC_INS ,KC_SCRL,
+          _______,KC_MPRV,KC_MPLY,KC_MNXT,KC_PSCR,_______,                        KC_VOLU,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,_______,
+          _______,_______,_______,_______,KC_HOME,KC_END,                         KC_VOLD,MS_LEFT,MS_DOWN,MS_UP  ,MS_RGHT,_______,
                           MS_BTN1,MS_BTN2,                                                        KC_F23 ,KC_F24 ,
                                                   _______,_______,        _______,_______,
                                                   _______,_______,        _______,_______,
